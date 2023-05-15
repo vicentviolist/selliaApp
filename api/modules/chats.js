c3ed74c138a5fa8da4e9d4183efa48c68f76db29
Vicente Olivares <vicent.violist@gmail.com>
2023-05-15 15:52:04 -0600

    clear api utils

--- api/modules/chats.js
+++ api/modules/chats.js
@@ -3,13 +3,3 @@ import apiFactory from "../apiFactory";
 export function getChtas(payload) {
   return apiFactory.gets(payload);
 }
-/* export function addTask(payload) {
-  return apiFactory.posts(payload);
-}
-export function deleteTask(payload) {
-  return apiFactory.deletes(payload);
-}
-
-export function updateTask(payload) {
-  return apiFactory.puts(payload);
-} */
